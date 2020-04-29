8ab14cb840c082032faf009071a93d26892157d8
Laurence James-Woodley <mr.lawjames@gmail.com>
2020-04-29 17:56:10 -0400

    read metadata via api

--- functions.R
+++ functions.R
@@ -25,12 +25,12 @@ get_records <- function(){
 get_checkboxes <- function(df){
   checkboxes <- data_dictionary %>%
     filter(field_type == "checkbox" & form_name == 'coronavirus_covid19_questionnaire') %>%  
-    select(variable_field_name, choices_calculations_or_slider_labels, field_label) %>%
-    separate_rows(choices_calculations_or_slider_labels, sep = "\\|") %>%
-    separate(choices_calculations_or_slider_labels, into = c("numeric_value", "label"),
+    select(field_name, select_choices_or_calculations, field_label) %>%
+    separate_rows(select_choices_or_calculations, sep = "\\|") %>%
+    separate(select_choices_or_calculations, into = c("numeric_value", "label"),
              sep = ",", extra = "merge") %>%
     mutate_all(trimws) %>% 
-    unite("checkbox_name", variable_field_name, numeric_value, sep = "___")
+    unite("checkbox_name", field_name, numeric_value, sep = "___")
   
   
   checkbox_cols <- df %>%
@@ -57,24 +57,24 @@ get_checkboxes <- function(df){
 get_radio_cols <- function(df){
   radio <- data_dictionary %>%
   filter(field_type == "radio" & form_name == 'coronavirus_covid19_questionnaire') %>%  
-    select(variable_field_name, choices_calculations_or_slider_labels, field_label) %>%
-    separate_rows(choices_calculations_or_slider_labels, sep = "\\|") %>%
-    separate(choices_calculations_or_slider_labels, into = c("numeric_value", "label"),
+    select(field_name, select_choices_or_calculations, field_label) %>%
+    separate_rows(select_choices_or_calculations, sep = "\\|") %>%
+    separate(select_choices_or_calculations, into = c("numeric_value", "label"),
              sep = ",", extra = "merge") %>%
     mutate_all(trimws) 
   
   radio_cols <- df %>%
     select(matches(paste0(
-      "^(", paste(radio$variable_field_name, collapse = "|"),
+      "^(", paste(radio$field_name, collapse = "|"),
       ")"
     ))) %>% 
     colnames()
   
   df_with_radio_labels <- df %>%
     select(radio_cols) %>%  
-    mutate_all(as.character) %>% 
-    pivot_longer(radio_cols, names_to = "variable_field_name",
-                 values_to = "numeric_value") %>%      
+    mutate_all(as.character) %>%  
+    pivot_longer(radio_cols, names_to = "field_name",
+                 values_to = "numeric_value") %>%  
     inner_join(radio) %>%   
     count(field_label, label)
   
@@ -85,15 +85,15 @@ get_radio_cols <- function(df){
 get_yes_no_cols <- function(df){
 yes_no_labels <- data_dictionary %>%
   filter(field_type == 'yesno' & form_name == 'coronavirus_covid19_questionnaire') %>% 
-  distinct(variable_field_name, field_label) %>%    
+  distinct(field_name, field_label) %>%    
   mutate(Yes = '1', No = '0') %>% 
-  pivot_longer(-c(variable_field_name,field_label),
+  pivot_longer(-c(field_name,field_label),
                names_to = "label", values_to = "field_value") %>% 
   mutate_all(as.character)
 
 yes_no_cols <- df %>%
   select(matches(paste0(
-    "^(", paste(yes_no_labels$variable_field_name, collapse = "|"),
+    "^(", paste(yes_no_labels$field_name, collapse = "|"),
     ")"
   ))) %>%  
   colnames()
@@ -103,8 +103,8 @@ df_with_yes_no_labels <- df %>%
   mutate_if(is.logical, ~ if_else(. == TRUE, 1,
                                   if_else(. == FALSE, 0, NA_real_))) %>% 
   select_if(is.numeric) %>% 
-  pivot_longer(cols = everything(), names_to = "variable_field_name",
-               values_to = "field_value") %>% 
+  pivot_longer(cols = everything(), names_to = "field_name",
+               values_to = "field_value") %>%  
   mutate_all(as.character) %>%   
   inner_join(yes_no_labels) %>%  
   count(field_label, label)
