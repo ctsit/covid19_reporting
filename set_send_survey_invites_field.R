66d3b37dc17f99f06575723d546a3fe98eb647a0
Laurence James-Woodley <mr.lawjames@gmail.com>
2020-06-03 12:23:38 -0400

    choose only subjects with swab results and use stricter matching

--- set_send_survey_invites_field.R
+++ set_send_survey_invites_field.R
@@ -6,35 +6,17 @@ source("functions.R")
 
 
 records <- get_records()  
-
-
-# restore old email addresses ---------------------------------------------
-
-# TODO: Delete this section once data has been uploaded to redcap
-restore_ce_email <- records %>% 
-  filter(!is.na(ce_email_backup)) %>% 
-  mutate(ce_email = coalesce(ce_email, ce_email_backup)) %>% 
-  select(record_id, redcap_event_name, ce_email) %>% 
-
-write_csv(restore_ce_email, "output/restore_ce_email.csv")
-
-
-# set send_survey_invites --------------------------------------------------------
-
-# TODO: IMPORTANT - Upload restore_ce_email.csv before doing this
-
+  
 filtered_records <- records %>% 
   mutate(test_date_and_time = as_date(test_date_and_time),
-         days_since_appt = today() - test_date_and_time) %>% 
-  filter(!is.na(covid_19_swab_result) |
-           (days_since_appt > 7 & 
-              is.na(covid_19_swab_result) &
-              test_date_and_time != '1969-12-31')) %>%
-  filter(redcap_event_name == "baseline_arm_1" & !is.na(send_survey_invites)) %>%
+         days_since_appt = today() - test_date_and_time) %>%
+  # TODO: filter(!is.na(survey_invite)) on future iterations
+  filter(redcap_event_name == "baseline_arm_1") %>%
+  filter(!is.na(covid_19_swab_result)) %>%
   filter(!is.na(ce_email)) %>%
   mutate_at(vars("ce_firstname", "ce_lastname", "ce_email"), tolower) %>% 
   select(record_id, redcap_event_name, ce_firstname, ce_lastname, patient_dob, 
-         ce_email)
+         ce_email, zipcode, q_agency)
 
 # get all unique emails within filtered_records
 unique_email <- filtered_records %>% 
@@ -48,30 +30,28 @@ one_subject_one_email <- unique_email %>%
 
 # of all unique emails did the same person use multiple emails
 one_subject_multiple_email <- unique_email %>% 
-  group_by(ce_firstname, ce_lastname) %>% 
+  group_by(ce_firstname, ce_lastname, patient_dob) %>% 
   filter(n() > 1) %>% 
   arrange(record_id) %>% 
   mutate(row_id = sequence(n())) %>% 
   mutate(send_survey = if_else(row_id == 1, 1, NA_real_))
 
 send_survey_field_set_by_unique_email <- one_subject_one_email %>% 
-  bind_rows(one_subject_multiple_email)
+  bind_rows(one_subject_multiple_email) 
 
 # when the emails are not unique within filtered_records...
 non_unique_emails <- filtered_records %>% 
   anti_join(send_survey_field_set_by_unique_email, by = "record_id")
 
-one_dob_one_email <- non_unique_emails %>% 
-  group_by(patient_dob, ce_email) %>% 
+one_dob_one_email <- non_unique_emails %>%
+  group_by(patient_dob, ce_email) %>%
   filter(n() == 1) %>% 
-  group_by(ce_firstname, ce_lastname) %>% 
-  arrange(record_id) %>% 
-  mutate(row_id = sequence(n())) %>% 
+  mutate(row_id = sequence(n())) %>%
   mutate(send_survey = if_else(row_id == 1, 1, NA_real_))
 
 one_dob_multiple_email <- non_unique_emails %>% 
   group_by(patient_dob, ce_email) %>% 
-  filter(n() > 1) %>% 
+  filter(n() > 1) %>%  
   arrange(record_id) %>% 
   mutate(row_id = sequence(n())) %>% 
   mutate(send_survey = if_else(row_id == 1, 1, NA_real_)) %>% 
@@ -80,7 +60,7 @@ one_dob_multiple_email <- non_unique_emails %>%
 # find subjects that were counted in one_dob_one_email and one_dob_multiple_email
 subjects_counted_twice <- one_dob_one_email %>% 
   bind_rows(one_dob_multiple_email) %>% 
-  group_by(ce_firstname, ce_lastname) %>% 
+  group_by(ce_firstname, ce_lastname, zipcode) %>% 
   mutate(send_survey_sum = sum(send_survey, na.rm = T)) %>% 
   filter(send_survey_sum > 1) %>% 
   arrange(record_id) %>% 
@@ -93,18 +73,25 @@ send_survey_field_set_by_non_unique_email <- subjects_counted_twice %>%
               filter(!record_id %in% subjects_counted_twice$record_id)) %>% 
   bind_rows(one_dob_multiple_email %>% 
               filter(!record_id %in% subjects_counted_twice$record_id))
-
+  
 set_send_survey_invites <- send_survey_field_set_by_unique_email %>% 
   bind_rows(send_survey_field_set_by_non_unique_email) %>% 
   arrange(ce_firstname, ce_lastname) %>% 
   mutate(send_survey_invites = if_else(is.na(send_survey), 0, send_survey)) %>% 
-  ungroup() %>% 
-  select(record_id, redcap_event_name, send_survey_invites)
+  ungroup()
+
+# verify that send_survey_invites for multiple subjects with same name was correctly set
+verify_same_names <- set_send_survey_invites %>% 
+  add_count(ce_firstname, ce_lastname) %>% 
+  filter(n > 1)  
+
+write_csv(verify_same_names, "output/verify_same_names.csv", na = "")
 
 # must be true
 if (nrow(set_send_survey_invites) == nrow(filtered_records)) {
-  write_csv(set_send_survey_invites, paste("output/survey_field_set_", 
-                                         today(), ".csv"))
+  write_csv(set_send_survey_invites %>% 
+              select(record_id, redcap_event_name, send_survey_invites),
+            paste("output/survey_field_set_", today(), ".csv"), na = "")
   
 }
 
